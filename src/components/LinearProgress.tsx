f3157c3d33fd1824f67b87afa5c1c33f6f988f34
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-27 16:12:34 +0700

    fix(components): fixing style of linearprogress color

--- src/components/LinearProgress.tsx
+++ src/components/LinearProgress.tsx
@@ -1,7 +1,7 @@
 const LinearProgress = () => {
   return (
-    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-slate-200 fixed">
-      <div className="bg-blue-600 h-2.5 rounded-full animate-loader absolute"></div>
+    <div className="w-full bg-indigo-400 rounded-full h-2.5 dark:bg-slate-200 fixed">
+      <div className="bg-indigo-500 h-2.5 rounded-full animate-loader absolute"></div>
     </div>
   );
 };
